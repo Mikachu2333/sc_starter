c81b6428b34c1abebba606cfae2e10b92bec0361
Mikachu2333 <mikachu.23333@zohomail.com>
2025-10-26 20:15:27 +0800

    update deps

--- build.rs
+++ build.rs
@@ -1,3 +1,5 @@
+extern crate tauri_winres as winres;
+
 fn main() {
     let time = vergen::BuildBuilder::default()
         .build_timestamp(true)
@@ -10,4 +12,16 @@ fn main() {
         .unwrap()
         .emit()
         .unwrap();
+
+    #[cfg(windows)]
+    {
+        let mut res = winres::WindowsResource::new();
+        res.set("FileDescription", "Starter for ScreenCapture");
+        res.set("LegalCopyright", "Copyright (C) Mikachu2333 2025. MIT License.");
+
+        if let Err(e) = res.compile() {
+            eprintln!("{e}");
+            std::process::exit(1);
+        }
+    }
 }
